b3f9f54be65cec8e0ee06417fd276a71ba84c48d
myrean <dhughes1@uci.edu>
2019-08-10 22:40:30 -0700

    added numbers allowed to username validation during registration

--- routes/auth/auth.routes.js
+++ routes/auth/auth.routes.js
@@ -15,7 +15,7 @@ const smtpTransport = require('../../controllers/nmController');
 router.post("/register", [
     //validate required fields from registration page
     check('email', 'Email address must be formatted correctly.').not().isEmpty().isEmail().normalizeEmail(),
-    check('username', 'Username Does Not Meet Requirements').escape().matches(/(^$)|^[.a-zA-Z\s-]+$]{0,8}$/),
+    check('username', 'Username Does Not Meet Requirements').escape().matches(/(^$)|^[.0-9a-zA-Z\s-]+$]{0,8}$/),
     //Password: min 5 char, max 24. one uppercase. one lower case. one special character. @#*!$%+=()
     check('password', 'Password does not meet the requirements.').not().isEmpty().trim().escape().matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#*!$%+=()])[0-9a-zA-Z@#*!$%+=()]{5,24}$/, "i"),
     check('verifyPassword', 'Passwords do not match').custom((value, { req }) => (value === req.body.password)),
